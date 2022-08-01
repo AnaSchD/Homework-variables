690d5023661b7141ac87f7be452fce650d4cdd28
Scherbakova <nastyasch0806@gmail.com>
2022-08-01 23:14:44 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,32 @@ public class Main {
         double boxerWeightDifference = twoBoxerWeight - oneBoxerWeight;
         System.out.println("Разница весов между боксерами " + boxerWeightDifference + " кг");
 
+        //Задание 3
+        byte bananas = 5;
+        byte oneBananasWeight = 80;
+        int allBananas = (bananas * oneBananasWeight);
+        System.out.println("Общий вес всех бананов " + allBananas + " гр");
+       short milk = 200;
+       double oneHundredOfMilkInGrams = 1.05;
+        double allMilk = (oneHundredOfMilkInGrams * milk);
+        System.out.println("Общий вес молока " + allMilk + " гр");
+        byte iceCream = 2;
+        byte oneIceCream = 100;
+        int allIceCreamWeight = oneIceCream * iceCream;
+        System.out.println("Общий вес мороженого "+ allIceCreamWeight + " гр");
+        byte eggs = 4;
+        byte oneEggsWeight = 70;
+        int allEggsWeight = eggs * oneEggsWeight;
+        System.out.println("Общий вес яиц " + allEggsWeight + " гр");
+       Double totalBreakfastWeight = allMilk + allBananas + allEggsWeight + allIceCreamWeight;
+        System.out.println("Общий вес спорт-завтрака в граммах " + totalBreakfastWeight +" гр");
+        short oneKgGr = 1000;
+        double conversationKg = totalBreakfastWeight / oneKgGr;
+        System.out.println("Вес переведенной еды из граммов в килограммы " + conversationKg + " кг");
+//Задание 4
+
+
+
 
 
 
