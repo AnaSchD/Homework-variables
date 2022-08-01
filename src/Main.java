46e073241871609d55a0ce9fd9edc3c215f2de0c
Scherbakova <nastyasch0806@gmail.com>
2022-08-01 22:01:22 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+   // Заданиние 1
+        byte c = 2;
+        short d = 5;
+        int p = 25;
+        long y = 365L;
+        float saltWeight = 23.2f;
+        double peppersWeight = 15.7;
+        boolean catIsAduIt = c > 12;
+        char g = 37;
+        System.out.println(catIsAduIt);
+
+
+
+
+
+
+
+
+
     }
 }
